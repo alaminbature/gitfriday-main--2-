c8f01a08f2c2376723a0d6d6a6634652478f42af
alamin bature <baturealamin4@gmail>
2023-11-10 11:00:02 +0100

    bature

---
+++ gitfriday-main/al_ameen_original.py
@@ -0,0 +1,6 @@
+name = "Al-Ameen Bature" 
+
+if name == "Al-Ameen Bature":
+    print("Original detected 😀")
+else:
+    print("Photocopy detected 😪")
---
+++ gitfriday-main/muhammad.py
@@ -0,0 +1,3 @@
+remark= "Alhamdulilah today is friday"
+prayers='may our lives should be at peace in this world and here after Ameen'
+l="jummat kareem" 
